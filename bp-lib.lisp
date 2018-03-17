9e4d958dcfdc8711a53b0ec4bade4fee483596dd
Michael MacLeod <michaelmmacleod@gmail.com>
2018-03-16 19:50:44 -0700

    Add --help

--- bp-lib.lisp
+++ bp-lib.lisp
@@ -16,6 +16,7 @@
            :make-parser
            :read-token
            :parse-standard-input
+           :--help
            :--make-parser
            :--list-parsers
            :--parse-token
@@ -93,6 +94,48 @@ with their corresponding values in parser."
           append (read-token parser) into xs
         else
           collect curr into xs))
+
+(defun --help ()
+  "Displays commandline option help."
+  (princ
+"Usage: bp --make-parser PARSER
+       bp --list-parsers
+       bp PARSER [PARSER-OPTION ..]
+
+Create and run file parsers.
+
+  --make-parser PARSER            Create a new parser. Parsers are located in
+                                  ~/.config/blueprint/parsers/.
+
+  --list-parsers                  Lists all parsers. This does not include
+                                  standalone parsers created with
+                                  --make-executable.
+
+PARSER-OPTION
+
+  PARSER --set-token TOKEN VALUE  Make #[TOKEN] parse to VALUE.
+
+  PARSER --list-tokens            List all tokens.
+
+  PARSER --run                    Read from standard input until EOF, parsing
+                                  it.
+                                    $ ./bp my-parser --list-tokens
+                                    my-token: world
+                                    $ cat parse-me
+                                    Hello, #[my-token]!
+                                    $ cat parse-me | ./bp my-parser --run
+                                    Hello, world!
+
+  PARSER --make-executable        Create a standalone executable parser.
+                                    $ cat parse-me
+                                    Hello, #[my-token]!
+                                    $ cat parse-me | ./bp my-parser --run
+                                    Hello, world
+                                    $ ./bp my-parser --make-executable
+                                    $ cat parse-me | ./my-parser
+                                  The executable does not need to be supplied
+                                  --run to parse its input.
+"))
 
 (defun --make-parser ()
   "Runs make-parser if the proper commandline options are supplied."
--- bp.lisp
+++ bp.lisp
@@ -14,6 +14,7 @@
   (--list-tokens)
   (--set-token)
   (--make-parser)
-  (--make-executable))
+  (--make-executable)
+  (--help))
 
 (read-args)
